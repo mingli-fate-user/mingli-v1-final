6a0add5d90b3700f2c8c7e9b034802363537ab13
mingli-fate-user <3376787168@qq.com>
2026-06-08 02:22:28 +0800

    金钱卦：内联yijing64数据，移除运行时fetch

--- assets/Culture-CGkb2uXj.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./react-vendor-8zYVP86P.js";import{C as t,c as s}from"./index-9_Yvw7el.js";import"./vendor-C-UA95kO.js";import"./router-D8aZx666.js";import"./icons-BLmnhXeQ.js";function e(){return r.jsx(t,{"code-path":"src/pages/Culture.tsx:5:10",pages:[s],basePath:"/culture",category:"术数文化"})}export{e as default};
